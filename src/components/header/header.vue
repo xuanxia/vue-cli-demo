713f3c5dcf899c2aba68d7a56f8e73d3f2585d96
xuanxia <1243228706@qq.com>
2016-11-24 21:10:05 +0800

    样式

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -29,14 +29,34 @@
     <div class="background">
       <img :src="seller.avatar" width="100%" height="100%">
     </div>
-    <div v-show="detailShow" class="detail">
+    <div v-show="detailShow" class="detail" transition="fade">
       <div class="detail-wrapper clearfix">
         <div class="detail-main">
           <div class="name">{{seller.name}}</div>
           <div class="star-warpper">
             <star :size="48" :score="seller.score"></star>
           </div>
+          <div class="detail-title">
+            <div class="detail-line"></div>
+            <div class="detail-text">优惠信息</div>
+            <div class="detail-line"></div>
+          </div>
+          <ul v-if="seller.supports" class="supports">
+            <li class="support-item" v-for="item in seller.supports">
+              <span class="icon" :class="classMap[item.type]"></span>
+              <span class="text">{{item.description}}</span>
+            </li>
+          </ul>
 
+          <!-- TODO 抽成组件-->
+          <div class="detail-title">
+            <div class="detail-line"></div>
+            <div class="detail-text">商家公告</div>
+            <div class="detail-line"></div>
+          </div>
+          <div class="bulletin">
+              <p class="content">{{seller.bulletin}}</p>
+          </div>
         </div>
       </div>
       <div class="detail-close" @click="closeDetail">
@@ -192,7 +212,14 @@
       width: 100%
       height: 100%
       overflow: auto
+      transition: all 0.7s
       background: rgba(7,17,27,0.8);
+      &.fade-transition
+        opacity: 1
+        background: rgba(7,17,27,0.8);
+      &.fade-enter,&.fade-leave
+        opacity: 0
+        background:  rgba(7,17,27,0)
       .detail-wrapper
         width: 100%
         min-height: 100%
@@ -207,12 +234,63 @@
           .star-warpper
             margin: 10px auto
             text-align: center
+          .detail-title
+            display: flex
+            width: 80%
+            margin: 28px auto 24px auto
+            .detail-line
+              flex: 1
+              position: relative
+              top: -6px
+              border-bottom: 1px solid rgba(255,255,255,0.2)
+            .detail-text
+              padding: 0 12px
+              font-size: 14px
+              font-weight: 700
+          .supports
+            width: 80%
+            margin: 0 auto
+            .support-item
+              padding: 0 12px
+              margin-bottom: 12px
+              font-size: 0
+              &:last-child
+                margin-bottom: 0
+              .icon
+                display: inline-block
+                width: 16px
+                height: 16px
+                vertical-align: top
+                margin-right: 6px;
+                background-size: 16px 16px
+                background-repeat: no-repeat
+                &.decrease
+                  bg-image('decrease_2')
+                &.discount
+                  bg-image('discount_2')
+                &.guarantee
+                  bg-image('guarantee_2')
+                &.invoice
+                  bg-image('invoice_2')
+                &.special
+                  bg-image('special_2')
+              .text
+                line-height: 16px
+                font-size: 12px
+          .bulletin
+            width: 80%
+            margin: 0 auto
+            .content
+              padding 0 12px
+              line-height: 24px
+              font-size: 12px
 
       .detail-close
         position: relative
-        width: 32px
+        width: 60px
         height: 32px
         margin: -64px auto 0 auto
         clear: both
         font-size: 32px
+        text-align: center
 </style>
