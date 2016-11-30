cc54b5792292b7cc2055fe33d5b9d7500d52da67
xuanxia <1243228706@qq.com>
2016-11-30 16:15:37 +0800

    购物车

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -2,7 +2,7 @@
   <div class="goods">
     <div class="menu-wrapper" v-el:menu-wrapper>
       <ul>
-        <li v-for="item in goods" class="menu-item">
+        <li v-for="item in goods" class="menu-item" :class="{'current':currentIndex === $index}" @click="mapDetali($index,$event)">
           <span class="text border-1px">
             <span v-show="item.type > 0" class="icon" :class="classMap[item.type]"></span><span>{{item.name}}</span>
           </span>
@@ -11,7 +11,7 @@
     </div>
     <div class="foods-wrapper" v-el:foods-wrapper>
       <ul>
-        <li v-for="item in goods" class="goods-list">
+        <li v-for="item in goods" class="goods-list food-list-hook">
           <h1 class="title">{{item.name}}</h1>
           <ul>
             <li v-for="food in item.foods" class="food-item border-1px">
@@ -33,11 +33,14 @@
         </li>
       </ul>
     </div>
+    <shopcart :delivery-price="seller.deliveryPrice" :min-price="seller.minPrice"></shopcart>
   </div>
+
 </template>
 
 <script type="text/ecmascript-6">
   import BScorll from 'better-scroll';
+  import shopcart from 'components/shopcart/shopcart';
   const ERR_OK = 0;
   export default {
     props: {
@@ -47,7 +50,9 @@
     },
     data () {
       return {
-        goods: {}
+        goods: {},
+        listHeight: [],
+        scrollY: 0
       };
     },
     created () {
@@ -58,16 +63,56 @@
           this.goods = responseData.data;
           this.$nextTick(() => {
             this._initScroll();
+            this._calculateHeight();
           });
-          console.log(this.goods);
-        };
+        }
       });
+    },
+    computed: {
+      currentIndex () {
+        for (let i = 0; i < this.listHeight.length; i++) {
+          let height1 = this.listHeight[i];
+          let height2 = this.listHeight[i + 1];
+          if (this.scrollY > height1 && this.scrollY < height2) {
+            return i;
+          }
+        }
+        return 0;
+      }
     },
     methods: {
       _initScroll () {
-        this.menuScroll = new BScorll(this.$els.menuWrapper, {});
-        this.foodsScroll = new BScorll(this.$els.foodsWrapper, {});
+        this.menuScroll = new BScorll(this.$els.menuWrapper, {
+          click: true
+        });
+        this.foodsScroll = new BScorll(this.$els.foodsWrapper, {
+          probeType: 3
+        });
+        this.foodsScroll.on('scroll', (pos) => {
+          this.scrollY = Math.abs(Math.round(pos.y));
+        });
+      },
+      _calculateHeight () {
+        let foodList = this.$els.foodsWrapper.getElementsByClassName('food-list-hook');
+        let height = -100;
+        this.listHeight.push(height);
+        for (let i = 0; i < foodList.length; i++) {
+          let item = foodList[i];
+          height += item.offsetHeight;
+          this.listHeight.push(height);
+        }
+      },
+      mapDetali (index, event) {
+        if (!event._constructed) {
+          return null;
+        }
+        let foodList = this.$els.foodsWrapper.getElementsByClassName('food-list-hook');
+        let el = foodList[index];
+        this.foodsScroll.scrollToElement(el, 300);
       }
+    },
+    components: {
+      shopcart: shopcart
     }
   };
 </script>
@@ -91,6 +136,13 @@
         height: 54px
         width: 56px
         line-height: 14px
+        &.current
+          position: relative
+          margin-top: -1px
+          z-index: 10
+          background: #fff
+          font-weight: 700
+          color: red
         .icon
           display: inline-block
           width: 16px
